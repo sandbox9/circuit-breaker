cee105bccbe35148f15459b2e9b3a730b5e01781
kyjin <yours012@gmail.com>
2015-02-09 15:45:12 +0900

    MyBatisDBCommandFilter 테스트케이스 작성

--- thor-connector-db-mybatis/src/test/java/thor/connector/db/mybatis/MybatisDbCommandFilterTest.java
+++ thor-connector-db-mybatis/src/test/java/thor/connector/db/mybatis/MybatisDbCommandFilterTest.java
@@ -1,55 +1,72 @@
 package thor.connector.db.mybatis;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
-import org.apache.ibatis.plugin.Interceptor;
-import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.MappedStatement.Builder;
 import org.apache.ibatis.plugin.Invocation;
-import org.apache.ibatis.plugin.Plugin;
-import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.Configuration;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import thor.connector.command.EventType;
+
+import com.netflix.config.ConfigurationManager;
+import com.netflix.hystrix.HystrixRequestLog;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 
 public class MybatisDbCommandFilterTest {
 
-
-	private MybatisDbCommandFilter filter;
-
 	@Test
-	public void initTest() throws Exception {
+	public void initTest() throws Throwable {
 
+		//		원래의 요청을 가로채(intercept) 호출하여 동일한 결과를 나타냄을 테스트하고, DBCommand를 이용하여 래핑했음을 확인한다.
+
+//		given
+		String statementId = "fineOne";
+		Object mockResult = new Object();
+
+		Invocation invocation = Mockito.mock(Invocation.class);
+		Mockito.when(invocation.proceed()).thenReturn(mockResult);
+
+		// final class라 Mock 객체를 생성할 수 없음 ...
+		Builder builder = new MappedStatement.Builder(new Configuration(), statementId, null, null);
+		MappedStatement mappedStatement = builder.build();
+		Mockito.when(invocation.getArgs()).thenReturn(new Object[]{mappedStatement});
+
+//		when
+		MybatisDbCommandFilter filter = new MybatisDbCommandFilter();
+		Object interceptedResult = filter.intercept(invocation);
+
+//		then
+		assertEquals("MyBatis 요청을 가로채 래핑하여 호출한 결과가 동일", mockResult, interceptedResult);
+
+		HystrixRequestLog currentRequest = HystrixRequestLog.getCurrentRequest();
+		assertNotNull(currentRequest);
+		assertTrue("DBCommand를 통해 요청이 처리되었는지 확인",
+				   currentRequest.getExecutedCommandsAsString().startsWith(EventType.DB + " : " + statementId));
+	}	
+	
+	
+	@Before
+	public void setUp() {
+		/* we must call this to simulate a new request lifecycle running and clearing caches */
+		HystrixRequestContext.initializeContext();
 	}
 
-	@Test
-	public void mapPluginShouldInterceptGet() {
-		Map map = new HashMap();
-		map = (Map) new AlwaysMapPlugin().plugin(map);
-		assertEquals("Always", map.get("Anything"));
-	}
-
-	@Test
-	public void shouldNotInterceptToString() {
-		Map map = new HashMap();
-		map = (Map) new AlwaysMapPlugin().plugin(map);
-		assertFalse("Always".equals(map.toString()));
-	}
-
-	@Intercepts({
-		@Signature(type = Map.class, method = "get", args = {Object.class})})
-	public static class AlwaysMapPlugin implements Interceptor {
-		public Object intercept(Invocation invocation) throws Throwable {
-			return "Always";
+	@After
+	public void tearDown() {
+		// instead of storing the reference from initialize we'll just get the current state and shutdown
+		if (HystrixRequestContext.getContextForCurrentThread() != null) {
+			// it could have been set NULL by the test
+			HystrixRequestContext.getContextForCurrentThread().shutdown();
 		}
 
-		public Object plugin(Object target) {
-			return Plugin.wrap(target, this);
-		}
-
-		public void setProperties(Properties properties) {
-		}
+		// force properties to be clean as well
+		ConfigurationManager.getConfigInstance().clear();
 	}
-
 }
